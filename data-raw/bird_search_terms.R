34e4fefdc64cc109684f45ddb6f97967aef4f460
Al-Stu <61510008+Al-Stu@users.noreply.github.com>
2020-10-28 11:27:08 +0000

    added scientific names to bird_search_terms

--- data-raw/bird_search_terms.R
+++ data-raw/bird_search_terms.R
@@ -19,6 +19,7 @@ bird_search_terms <- tidyr::unnest(overlap_checked_names[ , c(3,1)],
   dplyr::rename(name = positiveSearchTerms) %>%
   dplyr::left_join(all_names[ , c('internalTaxonId', 'name', 'language')]) %>%
   birdDataScrapeR::regexSearchTerms() %>%
-  dplyr::left_join(overlap_checked_names)
+  dplyr::left_join(overlap_checked_names) %>%
+  dplyr::left_join(all_names[ , c('internalTaxonId', 'scientificName')])
 
 usethis::use_data(bird_search_terms, overwrite = TRUE)
