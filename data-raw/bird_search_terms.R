2272b3a4aaac7d93668c237b991d903923decdd2
Al-Stu <61510008+Al-Stu@users.noreply.github.com>
2020-12-04 12:57:08 +0000

    updated search terms data so that overlap is checked within search types rather than overall (e.g. the common name branta doesn't remove the scientific name branta bernicula)

--- data-raw/bird_search_terms.R
+++ data-raw/bird_search_terms.R
@@ -13,16 +13,7 @@ all_names <- tidyRedlist::tidied_all_birds %>%
 overlap_checked_names <- findOverlappingNames(all_names)
 usethis::use_data(overlap_checked_names, overwrite = TRUE)
 
-bird_search_terms <- tidyr::unnest(overlap_checked_names[ , c(3,1)],
-                              positiveSearchTerms) %>%
-  dplyr::rename(name = positiveSearchTerms) %>%
-  dplyr::left_join(all_names[ , c('internalTaxonId', 'name', 'language')]) %>%
-  unique() %>%
-  dplyr::mutate(language = ifelse(is.na(language),
-                           "English",
-                           language
-                           )
-         ) %>%
+bird_search_terms <- all_names %>%
   birdDataScrapeR::regexSearchTerms() %>%
   dplyr::left_join(overlap_checked_names) %>%
   dplyr::left_join(all_names[ , c('internalTaxonId', 'scientificName')]) %>%
